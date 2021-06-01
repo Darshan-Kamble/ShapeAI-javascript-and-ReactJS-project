cffd867675e2e47f3cfa7f0cd5777965a606d83b
Darshan-Kamble <85150651+Darshan-Kamble@users.noreply.github.com>
2021-06-01 10:55:55 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+      It War A Great ExperienceWith Shape AI. They Give Us Basic Knowledge About Development.
+        This was an amazing bootcamp taken up by Shaurya Sinha We covered
+        everything from Scratch including Javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
